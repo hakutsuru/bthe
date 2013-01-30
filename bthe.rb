800ebefcff7fafbfbb85a356c2f45f933e39c950
Sean Broderick <hakutsuru@mac.com>
2013-01-30 04:48:06 -0500

    extract towers and reporting into modules, refactor methods

--- bthe.rb
+++ bthe.rb
@@ -1,108 +1,13 @@
 require 'pp'
 
-
-class Node
-  attr_reader :disk, :state, :history
-  NUMBER_OF_DISKS = 4
+module Towering
   EMPTY_CHARACTER = "x"
   TOWER_DELIMITER = "-"
-  BRANCH_MODE = "standard" # [standard, reverse]
-  SEEK_MODE = "lazy" # [lazy, exhaustive]
-  NARRATION = "none" # [none, verbose]
-  # child == potential new node in solution tree
-  # towers == state of puzzle, collection of arrays
-  Child = Struct.new(:disk_moved, :new_state, :node)
   Towers = Struct.new(:one, :two, :three)
-
-  def initialize (disk_moved, state, parent)
-    $nodes_created += 1
-    if NARRATION == "verbose"
-      pp "node: #{$nodes_created}"
-      pp "state: #{state}"
-    end
-    if $nodes_created == 1000 && $solutions_found == 0
-      pp "Warning: 1000 nodes created without solution found..."
-      pp "consider altering branch traversal mode."
-      pp "[stack limit will be exceeded, depending on context"
-      pp "this may seem like an infinite loop.]"
-      puts "\n"
-    end
-    # initialize instance
-    @disk = disk_moved
-    @state = state
-    @parent = parent
-    if parent
-      @history = parent.history.dup << state
-    else # root node
-      @history = [] << state
-    end
-    if (state != $solved_state)
-      # mixing metaphors, branch offsprings
-      @brood = analyze_options(@disk, @state, @history)
-    else
-      @brood = []
-    end
-    if NARRATION == "verbose"
-      pp "brood:"
-      pp @brood
-      puts "\n"
-    end
-  end
-
-  # [meta: 04]
-  def play
-    case
-    when state == $solved_state
-      # report solution, improve via method?
-      $solutions_found += 1
-      puts "Solution ##{$solutions_found}"
-      puts @history
-      puts "\n"
-      if SEEK_MODE == "lazy"
-        puts "Puzzle solved!"
-        puts "[finis]"
-        abort
-      end
-      @parent.prune
-    when @brood == []
-      @parent.prune
-    else
-      move = pick_branch
-      move.node = Node.new(move.disk_moved, move.new_state, self)
-      move.node.play
-    end
-  end
-  
-  # [meta: 03]
-  def self.solve_puzzle
-    # ugly, introduce puzzle object?
-    $nodes_created = 0
-    $solutions_found = 0
-    full_tower = (1..NUMBER_OF_DISKS).to_a
-    towers = Towers.new
-    # initial puzzle state
-    towers.one = full_tower
-    towers.two = []
-    towers.three = []
-    $initial_state = serialize_towers(towers)
-    # solved puzzle state
-    towers.one = []
-    towers.two = []
-    towers.three = full_tower
-    $solved_state = serialize_towers(towers)
-    # clean up
-    towers = nil
-    # solve
-    root = Node.new(nil, $initial_state, nil)
-    root.play
-  end
-
   # string representation of game state
-  # puzzle object and node objects would share towers, module?
   # state  => "1234-xxxx-xxxx"
   # towers => #<struct Node::Towers one=[1, 2, 3, 4], two=[], three=[]>
 
-  # [meta: 01]
   def self.build_towers(state)
     towers = Towers.new
     towers.one = build_tower(state[0,NUMBER_OF_DISKS])
@@ -130,8 +35,136 @@ class Node
     end
     mock_tower.join
   end
+end
 
-  # [meta: 03]
+module Reporting
+  def self.announce
+    puts "bthe - Brutish Tower of Hanoi Exercise"
+    puts "Brute force solving Towers of Hanoi"
+    puts "  Number of disks: #{NUMBER_OF_DISKS}"
+    puts "  Seek mode: #{SEEK_MODE}"
+    if SEEK_MODE == "lazy"
+      puts "  -- halts when any solution found"
+    end
+    puts "  Branch mode: #{BRANCH_MODE}"
+    puts "  Narration: #{NARRATION}"
+    puts "\n"
+  end
+
+  def self.update(state, possible_moves)
+    if NARRATION == "verbose"
+      puts "node: #{$nodes_created}"
+      puts "state: #{state}"
+      puts "brood:"
+      pp possible_moves
+      puts "\n"
+    end
+  end
+
+  def self.publish(move_history)
+    puts "Solution ##{$solutions_found}"
+    move_history.each { |move| puts "  #{move}" }
+    puts "\n"
+  end
+
+  def self.stack_warning
+    if $nodes_created == 1000 && $solutions_found == 0
+      puts "Warning: 1000 nodes created without solution found..."
+      puts "consider altering branch traversal mode."
+      puts "[stack limit will be exceeded, depending on context"
+      puts "this may seem like an infinite loop.]"
+      puts "\n"
+    end
+  end
+
+  def self.retire
+    puts "Puzzle solved!"
+    puts "Moves Evaluated: #{$nodes_created-1}"
+    puts "[finis]"
+  end
+end
+
+class Puzzle
+  include Towering
+  include Reporting
+
+  def self.solve
+    Reporting.announce
+    $nodes_created = 0
+    $solutions_found = 0
+    full_tower = (1..NUMBER_OF_DISKS).to_a
+    towers = Towers.new
+    # initial puzzle state
+    towers.one = full_tower
+    towers.two = []
+    towers.three = []
+    $initial_state = Towering.serialize_towers(towers)
+    # solved puzzle state
+    towers.one = []
+    towers.two = []
+    towers.three = full_tower
+    $solved_state = Towering.serialize_towers(towers)
+    # clean up
+    towers = nil
+    # solve
+    root = Node.new(nil, $initial_state, nil)
+    root.play
+  end
+end
+
+class Node
+  include Towering
+  include Reporting
+  attr_reader :disk, :state, :history
+  Child = Struct.new(:disk_moved, :new_state, :node)
+  # child - potential new node in solution tree
+
+  def initialize(disk_moved, state, parent)
+    $nodes_created += 1
+    # initialize instance
+    @disk = disk_moved
+    @state = state
+    @parent = parent
+    if parent
+      @history = parent.history.dup << state
+    else # root node
+      @history = [] << state
+    end
+    if (state != $solved_state)
+      # mixing metaphors, branch offsprings
+      @brood = analyze_options(@disk, @state, @history)
+    else
+      @brood = []
+    end
+    Reporting.update(state, @brood)
+    Reporting.stack_warning
+  end
+
+  def play
+    puzzle_solved = (state == $solved_state)
+    no_valid_moves = (@brood == [])
+    case
+    when puzzle_solved
+      publish_solution
+      @parent.prune
+    when no_valid_moves
+      @parent.prune
+    else
+      move = pick_branch
+      move.node = Node.new(move.disk_moved, move.new_state, self)
+      move.node.play
+    end
+  end
+
+  def publish_solution
+    $solutions_found += 1
+    Reporting.publish(@history)
+    if SEEK_MODE == "lazy"
+      Reporting.retire
+      abort
+    end
+  end
+
   def pick_branch
     case
     when BRANCH_MODE == "standard"
@@ -139,14 +172,13 @@ class Node
     when BRANCH_MODE == "reverse"
       move = @brood.last
     else
-      move = @brood.last
+      move = @brood.first
     end
     move
   end
 
-  # [meta: 03]
   def prune
-    # removed evaluated branch
+    # remove evaluated branch
     @brood.reject! { |move| move.node != nil }
     # choose another branch
     move = pick_branch
@@ -162,39 +194,43 @@ class Node
     end
   end
 
-  # [meta: 02]
   def analyze_options(previous_disk, state, history)
     possible_moves = []
-    towers = Node.build_towers(state)
+    towers = Towering.build_towers(state)
     towers.each_with_index do |source_tower, source_index|
       game_disk = source_tower[0]
       if !game_disk.nil? && game_disk != previous_disk
         towers.each_with_index do |destination_tower, destination_index|
           destination_disk = destination_tower[0].to_i
-          if game_disk < destination_disk || destination_disk == 0
-            # beware shallow copy, do not #dup
-            mock_towers = Node.build_towers(state)
-            mock_towers[source_index].shift
-            mock_towers[destination_index].unshift(game_disk)
-            new_state = Node.serialize_towers(mock_towers)
-            redundant_state = history.include?(new_state)
-            if !redundant_state
+          destination_disk_larger = (game_disk < destination_disk)
+          destination_tower_empty = (destination_disk == 0)
+          if destination_disk_larger || destination_tower_empty
+            new_state = next_state(state, source_index, destination_index)
+            if !redundant_state?(new_state, history)
               possible_moves << Child.new(game_disk,new_state,nil)
             end
-            mock_towers = nil
           end
         end
       end
     end
-    # check for puzzle solution
-    possible_moves = scan_for_solution(possible_moves)
+    possible_moves = impose_solution(possible_moves)
     # array of decision tree children
     possible_moves
   end
 
-  # perhaps create redundant_state?(new_state)
+  def next_state(state, source_index, destination_index)
+    # beware shallow copy, do not #dup
+    mock_towers = Towering.build_towers(state)
+    game_disk = mock_towers[source_index].shift
+    mock_towers[destination_index].unshift(game_disk)
+    Towering.serialize_towers(mock_towers)
+  end
 
-  def scan_for_solution(possible_moves)
+  def redundant_state?(state, history)
+    history.include?(state)
+  end
+
+  def impose_solution(possible_moves)
     # if any move results in solved puzzle
     # other move options are irrelevant
     solved = false
@@ -208,52 +244,12 @@ class Node
   end
 end
 
-
 if $0 == __FILE__
-  # Meta: begin project with rough idea for puzzle node [move state]
-  # 01 - start easy, serialization and rod models 
-  # 02 - work through finding possible moves
-  # 03 - puzzle initialization, removing branches
-  # 04 - play [recognizing solution, branching, backtracing]
-  # narration and seek mode added for debugging
+  NUMBER_OF_DISKS = 3
+  BRANCH_MODE = "standard" # [standard, reverse]
+  SEEK_MODE = "lazy" # [lazy, exhaustive]
+  NARRATION = "none" # [none, verbose]
 
-  # [meta: 01]
-  # pp Node.build_tower("1234")
-  # pp Node.build_tower("23")
-  # pp Node.serialize_tower([2,3])
-  # pp towers = Node.build_towers("x234-xxxx-xxx1")
-  # towers.each_with_index do |tower, index|
-  #   pp index
-  #   pp tower
-  # end
-  # pp Node.serialize_towers(towers)
-
-  # [meta: 02]
-  # pp towers = Node.build_towers("x234-xxxx-xxx1")
-  # history = []
-  # history << "1234-xxxx-xxxx"
-  # pp history
-  # pp Node.analyze_options(towers,history)
-
-  # [meta: 03]
-  # Node.solve_puzzle
-  # Child = Struct.new(:disk_moved, :new_state, :node)
-  # this_one = Child.new(3,"xx12-xxxx-xx34",nil)
-  # this_two = Child.new(4,$solved_state,nil)
-  # groovy = [this_one, this_two]
-  # pp Node.scan_for_solution(groovy)
-
-  # Child = Struct.new(:disk_moved, :new_state, :node)
-  # dat_thing = Child.new(0,"junk", nil)
-  # this_one = Child.new(3,"xx12-xxxx-xx34",nil)
-  # this_two = Child.new(4,"xx12-xxxx-xx32",dat_thing)
-  # groovy = [this_one, this_two]
-  # pp groovy
-  # puts "\n"
-  # pp Node.prune(groovy)
-
-  Node.solve_puzzle
-  puts "Puzzle solved!"
-  puts "Moves Evaluated: #{$nodes_created-1}"
-  puts "[finis]"
+  Puzzle.solve
+  Reporting.retire
 end
